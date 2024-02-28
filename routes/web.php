74579784b2563092e82ff60eec751f3ccfda4e02
Almaas Izdihar <almaasizdihar@gmail.com>
2024-02-28 11:43:48 +0700

    feat: add edit feature

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/pegawai',[PegawaiController::class,'index']);
 Route::get('/pegawai/tambah',[PegawaiController::class,'tambah']);
 
 Route::post('/pegawai/store',[PegawaiController::class,'store']);
+
+Route::get('/pegawai/edit/{id}',[PegawaiController::class, 'edit']);
+
+Route::post('/pegawai/update',[PegawaiController::class, 'update']);
