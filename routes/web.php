b686ed6de1e3f8a30b44ef742cce72dfe108dd44
Eriko Syah <erikosyahputraf@gmail.com>
2024-02-28 10:12:10 +0700

    menambah fitur add

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 
 //route CRUD
 Route::get('/pegawai',[PegawaiController::class,'index']);
+
+Route::get('/pegawai/tambah',[PegawaiController::class,'tambah']);
+
+Route::post('/pegawai/store',[PegawaiController::class,'store']);
